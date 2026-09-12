1b5888d20ee06d1fa8078f268629d887d7ea046f
gabrielapprado <gabriela.prado1306@gmail.com>
2026-09-11 22:57:51 -0300

    fix: corrige linha duplicada

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,20 +1,19 @@
 import styles from './SectionHeader.module.css'
 
-interface SectionHeaderProps{
-    titulo: string,
-    descricao?: string
-    linha?:boolean
+interface SectionHeaderProps {
+  titulo: string
+  descricao?: string
+  linha?: boolean
 }
 
-function SectionHeader({titulo, descricao, linha}:SectionHeaderProps){
-    return(
-        <div className={styles.header}>
-            <h2 className="section-title">{titulo}</h2>
-            <hr className={styles.linha}/>
-            {linha && <hr className={styles.linha} />}
-            {descricao && <p className="section-description">{descricao}</p> }
-        </div>
-    )
+function SectionHeader({ titulo, descricao, linha = true }: SectionHeaderProps) {
+  return (
+    <div className={styles.header}>
+      <h2 className="section-title">{titulo}</h2>
+      {linha && <hr className={styles.linha} />}
+      {descricao && <p className="section-description">{descricao}</p>}
+    </div>
+  )
 }
 
 export default SectionHeader
